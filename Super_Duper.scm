7decf51b129742a71d2d79edbd30ff97c1970a9a
Jeff <jeffkahn@u.boisestate.edu>
2020-09-14 15:16:53 -0600

    in prog

--- Super_Duper.scm
+++ Super_Duper.scm
@@ -2,6 +2,11 @@
 
 (define (Super_Duper source count)
 	(if (null? source)
-		""
-		(cons (cons (car source)  (car source)) (Super_Duper(cdr source) count))))
+		source
+		(Copy_It source count)))
+(define (Copy_It source count)
+	(if (not (= 0 count))
+		(cons (car source) (Copy_It source (- count 1)))
+		(Super_Duper(cdr source) count)))
+
 (display(Super_Duper '(x y) 2))
